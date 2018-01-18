706b77bd8d59ca1e2b808e341dd0d77e61a7d651
seetha7 <seethal@help.edu.my>
2018-01-18 09:08:34 +0800

    First Draft
    First

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Test\n"<<endl;
+    cout<<"Making changes"<<endl;
+    cout<"Making changes 2"<<endl;
+    cout<<"Making changes 3"<<endl;
+    cout<<"Making changes 4"<<endl;
+    cout<<"Making changes 5"<<endl;
+    cout<<"Making changes 6"<<endl;
+    cout<<"Making changes 7"<<endl;
+    cout<<"Making changes 8"<<endl;
+    cout<<"2"<<endl;
+    cout<<"3"<<endl;
+    cout<<"4"<<endl;
+    cout<<"5"<<endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
